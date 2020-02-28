3c6789e54dc2f22dedeea21cb1906f4751020194
tail <xuyi19930401@qq.com>
2020-02-28 10:08:26 +0800

    小尾巴

--- js/index.js
+++ js/index.js
@@ -9,18 +9,18 @@ var swiper = new Swiper('.swiper-container', {
             switch (swiper.activeIndex) {
                 case 0: 
                     $('.nav_menu_mile').addClass('nav_menu').removeClass('nav_menu_mile');
-                    $('img').removeClass('animated fadeInUp').hide().addClass('animated fadeInUp').show();                    
+                    $('.motion').removeClass('animated fadeInUp').hide().addClass('animated fadeInUp').show();                    
                     break;
                 case 1: 
                 case 2:
                 case 3:
                 case 4: 
-                    $('img').removeClass('animated fadeInUp').hide().addClass('animated fadeInUp').show();
+                    $('.motion').removeClass('animated fadeInUp').hide().addClass('animated fadeInUp').show();
                     $('.nav_menu').addClass('nav_menu_mile').removeClass('nav_menu');
                     break;
             }
         }
     });
 
-    $('img').addClass('animated fadeInUp').show();
+    $('.motion').addClass('animated fadeInUp').show();
 });
